bed0213e3ed9e05937cf9a04865d05903b029790
John Abreu <jboy1020@gmail.com>
2016-04-14 13:16:16 -0400

    Updated to 2016 event

--- script/script.js
+++ script/script.js
@@ -56,7 +56,7 @@ document.getElementById('sponsorForm').onsubmit = formSubmit;
 
 function animateBits() {
     const BITS_ID = 'bits';
-    const BITS_TEXT = 'bits';
+    const BITS_TEXT = 'code';
     const BITS_ANIMATE_INTERVAL = 50.0;
     const BITS_ANIMATE_DURATION = 1500.0;
     var bitsTextTicks = 0;
